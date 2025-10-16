0c5b93bc4895fba6a6b5aa0645955df10e1ae73a
Durgasai <durgasai@example.com>
2025-10-16 12:53:47 +0530

    Split header into an component and link

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,9 @@
 import { HomePage } from "./pages/HomePage"
 import { Routes,Route } from "react-router"
 import { CheckoutPage } from "./pages/CheckoutPage"
+import { OrdersPage } from "./pages/OrdersPage"
+import { TrackingPage} from "./pages/TrackingPage" 
+
 function App() {
 
   return (
@@ -8,8 +11,11 @@ function App() {
     <Routes>
       <Route index element={<HomePage /> } />
       <Route path="checkout" element={<CheckoutPage /> } />
-    
+      <Route path="orders" element={<OrdersPage /> } />
+      <Route path="tracking" element={<TrackingPage /> } />
+
     </Routes>
+
   )
 }
 
